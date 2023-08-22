b3a0929a438f5f68a1f53b6d03e7a2d91008239f
loc <tanlocnguyen296@gmail.com>
2023-08-22 23:32:37 +0700

    add mf

--- recsystools/feature_extraction/latent_embedding.py
+++ recsystools/feature_extraction/latent_embedding.py
@@ -2,7 +2,7 @@ from typing import List, Tuple, Optional, Union
 
 import numpy as np
 from scipy.sparse import csr_matrix
-from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import TruncatedSVD,NMF
 
 
 def get_user_item_embedding_from_interaction_matrix_svd(
@@ -20,4 +20,15 @@ def get_user_item_embedding_from_interaction_matrix_svd(
     
     return U,V.T,svd
 
+def get_user_item_embedding_from_interaction_matrix_matrix_factorization(
+    interaction_matrix: Union[np.ndarray, csr_matrix],
+    n_components: int,
+    n_iter: int,
+    *args,
+    **kwargs,
+)->Tuple[np.ndarray,np.ndarray,NMF]:
+    model = NMF(n_components=n_components, init='random', random_state=0,max_iter=n_iter,*args,**kwargs)
+    W = model.fit_transform(interaction_matrix)
+    H = model.components_
     
+    return W, H.T,model
--- test/feature_extraction/test_latent_embedding.py
+++ test/feature_extraction/test_latent_embedding.py
@@ -14,7 +14,7 @@ def test_get_user_item_embedding_from_interaction_matrix_svd():
         items_embedding,
         svd,
     ) = latent_embedding.get_user_item_embedding_from_interaction_matrix_svd(
-        rand_interaction_matrix, n_components=5, n_iter=2
+        rand_interaction_matrix, n_components=5, n_iter=2,algorithm='arpack'
     )
 
     assert users_embedding.shape == (10, 5)
@@ -23,3 +23,28 @@ def test_get_user_item_embedding_from_interaction_matrix_svd():
     assert (
         users_embedding @ np.diag(svd.singular_values_) @ items_embedding.T
     ).shape == (10, 20)
+    
+
+
+def test_get_user_item_embedding_from_interaction_matrix_matrix_factorization():
+    n_users = 10
+    n_items = 20
+
+    rand_interaction_matrix = np.random.rand(n_users, n_items)
+
+    (
+        users_embedding,
+        items_embedding,
+        nmf,
+    ) = latent_embedding.get_user_item_embedding_from_interaction_matrix_matrix_factorization(
+        rand_interaction_matrix, n_components=5, n_iter=2
+    )
+
+    assert users_embedding.shape == (10, 5)
+    assert items_embedding.shape == (20, 5)
+
+    assert (
+        users_embedding @ items_embedding.T
+    ).shape == (10, 20)
+    
+    
